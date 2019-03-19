4e52c6c37afb5585085aee46f9b42a547704d941
Jackieh55555 <jackie.huang.ca@gmail.com>
2019-03-18 20:35:27 -0400

    Server

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const http      = require('http');
 const server    = http.createServer(app);
 const socketIO  = require('socket.io')(server); //hello I am new
 
-const LISTEN_PORT = 3030; //make sure greater than 3000. Some ports are reserved/blocked by firewall ...
+const LISTEN_PORT = 8080; //make sure greater than 3000. Some ports are reserved/blocked by firewall ...
 
 app.use((express.static(__dirname + '/public'))); //set root dir to the public folder
 
 //routes
-app.get('/color', function(req,res) {
+app.get('/', function(req,res) {
     res.sendFile(__dirname + '/public/color.html');
 });
 
-app.get('/controller', function(req,res) {
+app.get('/', function(req,res) {
     res.sendFile(__dirname + '/public/controller.html');
 });
 
