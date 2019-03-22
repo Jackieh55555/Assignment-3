7a23c4821cbd223c5ed94725f5b3050f444c769a
Jackieh55555 <jackie.huang.ca@gmail.com>
2019-03-21 22:43:26 -0400

    Attempts with arrays

--- app.js
+++ app.js
@@ -17,51 +17,109 @@ app.get('/controller', function(req,res) {
     res.sendFile(__dirname + '/public/controller.html');
 });
 
+
+// Array for available colours, playerId, player selected colour, etc
+let nextPlayerId = 0;
+let numberPlayersInGame = 0;
+
+let playerId = {};
+let playerColour = {};
+
+let colours = {
+    red:    {r:255, g:0,   b:0  },
+    orange: {r:255, g:127, b:80 },
+    yellow: {r:255, g:255, b:0  },
+    green:  {r:0,   g:128, b:0  },
+    blue:   {r:0,   g:191, b:255},
+    purple: {r:139, g:0,   b:255},
+    pink:   {r:255, g:105, b:180}
+};
+
+
 //websocket stuff
 socketIO.on('connection', function(socket) {
-    console.log(socket.id + 'A player has connected to the game!');
+    nextPlayerId++;
+    numberPlayersInGame++;
+    playerId[socket.id] = nextPlayerId;
+    console.log('player#' + playerId[socket.id] + ' (socket.id=' + socket.id + ') has joined the game! => Total=' + numberPlayersInGame);
+    console.log(playerId);
 
     socket.on('disconnect', function(data) {
-        console.log(socket.id + 'Player has disconnected game');
+        numberPlayersInGame--;
+        console.log('player#' + playerId[socket.id] + ' (socket.id=' + socket.id + ') has left the game! => Total=' + numberPlayersInGame);
+        delete playerId[socket.id];
+        delete playerColour[socket.id];
+        console.log(playerId);
     });
+
 
     //custom events
     //socket = one client
     //socketIO.sockets = all clients
+
+    socket.on('colour_selected', function(data) {
+        console.log('player#' + playerId[socket.id] + ' selected colour ' + data);
+        playerColour[socket.id] = colours[data];
+        console.log('# of players selected colours are: ' + Object.keys(playerColour).length)
+        console.log(playerColour);
+        socketIO.sockets.emit('color_change', colours[data]);
+    });
+
     socket.on('red', function(data) {
-        console.log('Red event heard');
+        //console.log('Red event heard');
+        console.log('Red event heard from player#' + playerId[socket.id]);
+        playerColour[socket.id] = {r:255, g:0, b:0};
+        console.log(playerColour);
         socketIO.sockets.emit('color_change', {r:255, g:0, b:0});
     });
 
     socket.on('orange', function(data) {
-        console.log('Orange event heard');
+        //console.log('Orange event heard');
+        console.log('Orange event heard from player#' + playerId[socket.id]);
+        playerColour[socket.id] = {r:255, g:127, b:80};
         socketIO.sockets.emit('color_change', {r:255, g:127, b:80});
     });
 
     socket.on('yellow', function(data) {
-        console.log('Yellow event heard');
+        //console.log('Yellow event heard');
+        console.log('Yellow event heard from player#' + playerId[socket.id]);
+        playerColour[socket.id] = {r:255, g:255, b:0};
+        console.log(playerColour);
         socketIO.sockets.emit('color_change', {r:255, g:255, b:0});
     });
 
     socket.on('green', function(data) {
-        console.log('Green event heard');
+        //console.log('Green event heard');
+        console.log('Green event heard from player#' + playerId[socket.id]);
+        playerColour[socket.id] = {r:0, g:128, b:0};
+        console.log(playerColour);
         socketIO.sockets.emit('color_change', {r:0, g:128, b:0});
     });
 
     socket.on('blue', function(data) {
-        console.log('Blue event heard');
+        //console.log('Blue event heard');
+        console.log('Blue event heard from player#' + playerId[socket.id]);
+        playerColour[socket.id] = {r:0, g:191, b:255};
+        console.log(playerColour);
         socketIO.sockets.emit('color_change', {r:0, g:191, b:255});
     });
 
     socket.on('purple', function(data) {
-        console.log('Purple event heard');
+        //console.log('Purple event heard');
+        console.log('Purple event heard from player#' + playerId[socket.id]);
+        playerColour[socket.id] = {r:139, g:0, b:255};
+        console.log(playerColour);
         socketIO.sockets.emit('color_change', {r:139, g:0, b:255});
     });
 
     socket.on('pink', function(data) {
-        console.log('Pink event heard');
+        //console.log('Pink event heard');
+        console.log('Pink event heard from player#' + playerId[socket.id]);
+        playerColour[socket.id] = {r:255, g:105, b:180};
+        console.log(playerColour);
         socketIO.sockets.emit('color_change', {r:255, g:105, b:180});
     });
+
 });
 
 //finally, start server
